faa6bb45421212f14ca4b7f55d248fe6aacb6837
森山啓輔 <>
2021-06-14 23:13:03 +0900

    first commit.

---
+++ service-disabler.bat
@@ -0,0 +1,113 @@
+net stop AxInstSV
+net stop Browser
+net stop CertPropSvc
+net stop DiagTrack
+net stop DusmSvc
+net stop Fax
+net stop fdPHost
+net stop FDResPub
+net stop gupdate
+net stop icssvc
+net stop lfsvc
+net stop MapsBroker
+net stop MSDTC
+net stop MSiSCSI
+net stop Netlogon
+net stop NetTcpPortSharing
+net stop p2pimsvc
+net stop p2psvc
+net stop PNRPAutoReg
+net stop PNRPsvc
+net stop PrintNotify
+net stop RasMan
+net stop RemoteAccess
+net stop RemoteRegistry
+net stop RetailDemo
+net stop SCardSvr
+net stop ScDeviceEnum
+net stop SCPolicySvc
+net stop seclogon
+net stop SensorDataService
+net stop SensorService
+net stop SensrSvc
+net stop SharedAccess
+net stop ShellHWDetection
+net stop shpamsvc
+net stop SNMPTRAP
+net stop Spooler
+net stop SSDPSRV
+net stop SstpSvc
+net stop stisvc
+net stop TermService
+net stop TrkWks
+net stop tzautoupdate
+net stop upnphost
+net stop WerSvc
+net stop WinHttpAutoProxySvc
+net stop WinRM
+net stop wmiApSrv
+net stop WMPNetworkSvc
+net stop WPDBusEnum
+net stop WSearch
+net stop XblAuthManager
+net stop XblGameSave
+net stop XboxGipSvc
+net stop XboxNetApiSvc
+
+sc config AxInstSV start=disabled
+sc config Browser start=disabled
+sc config CertPropSvc start=disabled
+sc config DiagTrack start=disabled
+sc config DusmSvc start=disabled
+sc config Fax start=disabled
+sc config fdPHost start=disabled
+sc config FDResPub start=disabled
+sc config gupdate start=disabled
+sc config icssvc start=disabled
+sc config lfsvc start=disabled
+sc config MapsBroker start=disabled
+sc config MSDTC start=disabled
+sc config MSiSCSI start=disabled
+sc config Netlogon start=disabled
+sc config NetTcpPortSharing start=disabled
+sc config p2pimsvc start=disabled
+sc config p2psvc start=disabled
+sc config PNRPAutoReg start=disabled
+sc config PNRPsvc start=disabled
+sc config PrintNotify start=disabled
+sc config RasMan start=disabled
+sc config RemoteAccess start=disabled
+sc config RemoteRegistry start=disabled
+sc config RetailDemo start=disabled
+sc config SCardSvr start=disabled
+sc config ScDeviceEnum start=disabled
+sc config SCPolicySvc start=disabled
+sc config seclogon start=disabled
+sc config SensorDataService start=disabled
+sc config SensorService start=disabled
+sc config SensrSvc start=disabled
+sc config SharedAccess start=disabled
+sc config ShellHWDetection start=disabled
+sc config shpamsvc start=disabled
+sc config SNMPTRAP start=disabled
+sc config Spooler start=disabled
+sc config SSDPSRV start=disabled
+sc config SstpSvc start=disabled
+sc config stisvc start=disabled
+sc config TermService start=disabled
+sc config TrkWks start=disabled
+sc config tzautoupdate start=disabled
+sc config upnphost start=disabled
+sc config WerSvc start=disabled
+sc config WinHttpAutoProxySvc start=disabled
+sc config WinRM start=disabled
+sc config wmiApSrv start=disabled
+sc config WMPNetworkSvc start=disabled
+sc config WPDBusEnum start=disabled
+sc config WSearch start=disabled
+sc config XblAuthManager start=disabled
+sc config XblGameSave start=disabled
+sc config XboxGipSvc start=disabled
+sc config XboxNetApiSvc start=disabled
+
+pause
